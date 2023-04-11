b2134f15ce625577bec1e290fbf51fbdea489729
ilomily <101515603+ilomily@users.noreply.github.com>
2023-04-11 14:51:04 +0300

    Add files via upload

---
+++ KdvHesaplama.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+        /*
+        * kdvsiz tutar:input
+        * input + input*0,18 = kdvli tutar
+        * input*0,18 = kdv tutarı
+        * */
+
+        double kdv;
+        double kdvliFiyat, kdvTutari, fiyat;
+
+        System.out.print("KDV'siz fiyat: ");
+
+        Scanner input = new Scanner(System.in);
+        fiyat =  input.nextDouble();
+
+        kdv = fiyat <= 1000 ? 0.18 : 0.08;
+        kdvTutari = fiyat*kdv;
+        kdvliFiyat = fiyat + kdvTutari;
+
+        System.out.println("KDV Oranı: "+kdv);
+        System.out.println("KDV'li Fiyat: " + kdvliFiyat);
+        System.out.println("KDV Tutarı: " + kdvTutari);
+
+
+
+
+
+    }
+}
